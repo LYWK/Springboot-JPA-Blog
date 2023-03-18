b89904ffbcf9864c4be84e2b6f6b1f4112921ade
DS007730 <lywk@softworks.co.kr>
2023-03-19 00:18:21 +0900

    js설명

--- src/main/resources/static/js/user/user.js
+++ src/main/resources/static/js/user/user.js
@@ -1,6 +1,6 @@
 let index = {
 	init: function(){
-		$("#btn-save").on("click", ()=>{
+		$("#btn-save").on("click", ()=>{ // arrow 사용 이유 => this를 바인딩....
 			this.save();
 		});
 	},
@@ -13,8 +13,12 @@ let index = {
 			email : $("#email").val()
 		}
 		console.log(data);
-		$.ajax().done().fail(); //ajax통신을 이용해서  3개의 데이터를 json으로 변경하여 insert 요청
+		$.ajax({}).done({}).fail({}); //ajax통신을 이용해서  3개의 파라미터를 json으로 변경하여 insert 요청
+		//ajax 1_요청에 대한 응답을 html이 아닌 data(json)로 받기 위해...
+		//     브라우져로 서버에 요청 ->  서버는 응답을 html로 함  보통은..... 
+		//   html은 browser가 이해함.  1. 서버를 하나로 통일(웹용 앱용 따로 필요가 없음-데이터통신 하니까 ..)
+		//2. 비동기 통신. ==>  ex)  
 	},
-}
+} //그냥 object
 
-index.init();
+index.init();// 함수 호출하고 함수 바인딩
