505f93d9d73543d9999b8ea2f5db36837b29718f
DS007730 <lywk@softworks.co.kr>
2023-03-22 17:36:15 +0900

    기본적인login방식_session

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,5 +1,7 @@
 package com.cos.blog.controller.api;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +19,8 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 	
+//	@Autowired
+//	private HttpSession session;
 	
 	@PostMapping("/api/user")
 	public ResponseDto<Integer> save(@RequestBody User user) {
@@ -26,5 +30,18 @@ public class UserApiController {
 		userService.회원가입(user);
 		
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);// java object 를 json으로 변환해서 리턴 (jackson)
+	}
+	
+	//기본적인 login 방식 - 브라우져에서 넘어온 user정보를 세션에 담아서 응답 데이터로 넘겨줌... 
+	@PostMapping("/api/user/login")
+	public ResponseDto<Integer> login(@RequestBody User user,HttpSession session){
+		System.out.println("UserApiController : login 호출됨.");
+		
+		User principal =  userService.로그인(user);
+		
+		if (principal != null) {
+			session.setAttribute("principal", principal);
+		}
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 }
--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.cos.blog.model.User;
 
 public interface UserRepository extends JpaRepository<User,Integer>{
-		
+		//JPA Naming query
+	// select * from user where username =  ? and password = ?
+	   User findbyUsernameAndPassword(String username, String password);
 }
+ 
